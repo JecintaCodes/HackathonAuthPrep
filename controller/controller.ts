eceb1b17d8206f9db2b6363e7e5b129fa627e222
UchennaAustine <uchennaaustine8@gmail.com>
2023-10-18 07:02:56 +0100

    add

---
+++ controller/controller.ts
@@ -0,0 +1,24 @@
+import { Response, Request } from "express";
+import { statusCode } from "../utils/statusCode";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const Register = async (req: Request, res: Response) => {
+  try {
+    const { email, password, name } = req.body;
+    const lock = await bcrypt.genSalt(10);
+    const encrypt = await bcrypt.salt(password, lock);
+    const token = await jwt.sign();
+
+    // const
+    return res.status(statusCode.CREATE).json({
+      message: `User Registration SuccessFul:`,
+      data: "",
+    });
+  } catch (error: any) {
+    return res.status(statusCode.BAD_REQUEST).json({
+      message: `User Registration Error: ${error.message}`,
+      info: error,
+    });
+  }
+};
